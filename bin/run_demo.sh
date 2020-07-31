64298961b690946fcf683fdcb1ac7e224984674e
Tim Jenness <tjenness@lsst.org>
2020-07-31 15:59:20 -0700

    Switch to using standard butler commands
    * Instrument is now registered
    * We do not import instrument dimensions
    * Visits are defined.

--- bin/run_demo.sh
+++ bin/run_demo.sh
@@ -8,30 +8,31 @@ set -e
 # Echo commands
 set -x
 
-# 
+#
 # Doing this as a shell script instead of scons to make it
 # easier to read.
 
 if [ ! -f DATA_REPO/butler.yaml ]; then
-    makeButlerRepo.py DATA_REPO
+    butler create DATA_REPO
+    butler register-instrument DATA_REPO lsst.obs.subaru.HyperSuprimeCam
 fi
 
-
-if [ ! -d DATA_REPO/calib ]; then
-    bin/ingestExternalData.py DATA_REPO $PWD/export_dir/export.yaml
+# Hack assuming posix datastore
+if [ ! -d DATA_REPO/HSC/calib ]; then
+    butler import DATA_REPO $PWD/export_dir --export-file $PWD/export_dir/export.yaml --output-run shared/ci_hsc --skip-dimensions instrument,physical_filter,detector
 fi
 
 # ingestRaws.py doesn't search recursively; over-specifying to work around that.
-if [ -z "$(find -L DATA_REPO/raw -type f)" ]; then
-    bin/ingestRaws.py DATA_REPO export_dir/raw/hsc/raw/r/HSC-R/903342/
+if [ -z "$(find -L DATA_REPO/HSC/raw -type f)" ]; then
+    butler ingest-raws DATA_REPO --dir export_dir/HSC/raw/all/raw/r/HSC-R/
+    butler define-visits DATA_REPO -i HSC --collections HSC/raw/all
 fi
 
 # I dropped the masks/hsc and skymaps collections; apparently they weren't
 # needed for this graph so they weren't exported?
 # Also this will fail upon re-running, because it wants `--extend-run` if and
 # only if the output run already exists.
-pipetask run -d "visit=903342 AND detector=10" -j 1 -b DATA_REPO/butler.yaml \
-    -i calib/hsc,raw/hsc,ref_cats,shared/ci_hsc \
+pipetask run -d "exposure=903342 AND detector=10" -j 1 -b DATA_REPO/butler.yaml \
+    -i HSC/calib,HSC/raw/all,ref_cats,shared/ci_hsc \
     --register-dataset-types -p $PIPE_TASKS_DIR/pipelines/ProcessCcd.yaml \
-    --instrument lsst.obs.subaru.HyperSuprimeCam --output-run demo_collection 
-
+    --instrument lsst.obs.subaru.HyperSuprimeCam --output-run demo_collection
