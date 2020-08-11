fb18c7fe81a1d7e4483e34b60cc5ed029beb7f4e
Tim Jenness <tjenness@lsst.org>
2020-08-11 09:58:27 -0700

    Add software license to scripts

--- bin/run_demo.sh
+++ bin/run_demo.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# This file is part of pipelines_check.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 export DYLD_LIBRARY_PATH=${LSST_LIBRARY_PATH}
 
 # Exit on command failure
@@ -8,7 +29,6 @@ set -e
 # Echo commands
 set -x
 
-#
 # Doing this as a shell script instead of scons to make it
 # easier to read.
 
